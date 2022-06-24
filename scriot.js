f60db6935e9e3a9d4cc6d39609899b9a87688075
muydinov7 <muydinovmuhammad41@gmail.com>
2022-06-24 08:28:04 +0500

    first commit

---
+++ scriot.js
@@ -0,0 +1,76 @@
+const tap = document.querySelector("#tap")
+const none = document.querySelector("#html_block")
+const blok = document.querySelector(".game-start")
+const bosh = document.querySelector("#bosh")
+const bosh2 = document.querySelector("#bosh2")
+const boshBut = document.getElementById("bosh_but")
+const win = document.getElementById("winner")
+const win2 = document.getElementById("winner2")
+const clear = document.getElementById("clear")
+const plne = document.getElementById("player_name")
+const plne2 = document.getElementById("player_name2")
+
+
+
+
+tap.addEventListener("click", function (){
+    none.style.display="none"
+    blok.style.display="block"
+})
+let player1;
+let player2;
+boshBut.addEventListener("click" , function (){
+    player1 = Math.floor(Math.random() * 100);
+    player2 = Math.floor(Math.random() * 100);
+
+    bosh.textContent = player1;
+    bosh2.textContent = player2;
+
+    if(player1 < player2){
+        win.append(player1 = "win")
+    }
+    if(player1 > player2){
+        win2.append(player2 = " win")
+    }
+    clear.addEventListener("click",function (){
+        win.remove();
+        win2.remove();
+    })
+    
+ 
+})
+
+// const pr = prompt("Player Name")
+// const pr2 = prompt("Player Name")
+
+
+// plne.innerText = pr;
+// plne2.innerText = pr2;
+
+
+
+// const yosh = 50;
+
+// if(yosh >= pr){
+//     console.log("maktabga kirdi")
+// }else if(yosh <= pr);{
+//     console.log("kirmadi");
+// }
+
+// const addnum = prompt("raqam kiriting")
+
+
+//  if(addnum == 7 ){
+//     console.log("########")
+
+//  }else if( addnum == 10){
+//     console.log("########")
+    
+//  }else if(addnum == 12){
+//     console.log("########")
+
+//  }
+
+ 
+
+    //bosh2.innerHTML = Math.floor(Math.random() * 100);
